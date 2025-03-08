3a51c47280faff18f6054afa08d0da31e0508654
krunid <64851441+krunid@users.noreply.github.com>
2025-03-08 19:55:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export default {
-    aprUrl: "https://script.google.com/macros/s/AKfycbxVUinvWzsp178oiD7ldBJFlc3J-Bke1377PsJ9uuBIbrKgI-TlxuO0xtzmqSV3af0b/exec"
+    aprUrl: "https://script.google.com/macros/s/AKfycbw4ba9fMeP6cWdDxCVDzVrmcc-rzoyfuw7782gyLV02RqQqhnNgpyXkp4C_lyN6STVtUw/exec"
 }
